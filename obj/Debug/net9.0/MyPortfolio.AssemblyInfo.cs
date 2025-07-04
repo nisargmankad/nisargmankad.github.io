7c219cd4d28e678c542c334e72e56fe7d486f405
Nash <nisarg.mankad@gmail.com>
2025-07-03 22:55:48 -0700

    message

--- obj/Debug/net9.0/MyPortfolio.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyPortfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d7c8d54f8e250fb9e7fd6605ca463adf67f3ec8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df1f12701796f0219b5fd46fd0470fdfb994e910")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
