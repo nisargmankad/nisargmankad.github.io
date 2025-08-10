dca3eaf9e3c8faf75d333ac74315c873d08f9b16
Nash <nisarg.mankad@gmail.com>
2025-08-10 14:04:15 -0700

    post publish

--- obj/Release/net9.0/MyPortfolio.AssemblyInfo.cs
+++ obj/Release/net9.0/MyPortfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14ef8fa5f7e9c3a1469877f0328768626f27c834")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
