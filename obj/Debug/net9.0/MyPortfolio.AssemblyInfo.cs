46d6fa5e7ea80239d20a8cd186aebb389ea54966
Nash <nisarg.mankad@gmail.com>
2025-08-09 20:18:08 -0700

    image fix

--- obj/Debug/net9.0/MyPortfolio.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyPortfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c2d0cbb7a58972f41fc75f8108c51a672820158")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e6f90230ab2798bd9f0d27eb7f70755b407dd2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
