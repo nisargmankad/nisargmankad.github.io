c555c1548789df9f11d39b3c85acc37804166739
Nash <nisarg.mankad@gmail.com>
2025-08-09 23:25:54 -0700

    not working

---
+++ Models/GithubEvent.cs
@@ -0,0 +1,30 @@
+namespace MyPortfolio.Models
+{
+    public class GithubEvent
+    {
+        public string? type { get; set; }
+        public DateTime created_at { get; set; }
+        public GithubEventPayload? payload { get; set; }
+    }
+
+    public class GithubEventPayload
+    {
+        public int size { get; set; } // number of commits in the push
+        public List<GithubCommit>? commits { get; set; }
+    }
+
+    public class GithubCommit
+    {
+        public string? sha { get; set; }
+        public GithubAuthor? author { get; set; }
+        public string? message { get; set; }
+        public bool? distinct { get; set; }
+        public string? url { get; set; }
+    }
+
+    public class GithubAuthor
+    {
+        public string? email { get; set; }
+        public string? name { get; set; }
+    }
+}
--- obj/Debug/net9.0/MyPortfolio.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyPortfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46d6fa5e7ea80239d20a8cd186aebb389ea54966")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c04fb83f6630c9abfb37d2ab446b95f185b03807")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
